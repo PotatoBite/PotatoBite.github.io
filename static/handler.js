546177a20a996522b958804c162b264b36938896
Darian Lopez Utra <daroxs95@gmail.com>
2020-06-12 14:22:33 -0700

    Update handler.js

--- static/handler.js
+++ static/handler.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
-//$("#code_description").load("code_description.txt");
-$("#code_description").text("code_description.txt");
+$("#code_description").load("code_description.txt");
+//$("#code_description").text("code_description.txt");
 
 
 });
