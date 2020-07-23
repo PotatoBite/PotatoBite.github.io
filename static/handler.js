954cd627444ebd5249aaffc13ad3a9860d91c3e2
Darian Lopez Utra <daroxs95@gmail.com>
2020-07-23 02:02:52 -0700

    tiny core CMS ready

--- static/handler.js
+++ static/handler.js
@@ -1,6 +1,37 @@
 $(document).ready(function() {
+    var temp_sections = getSections("static/content.txt");
 
-$("#code_description").load("static/code_description.txt");
+    /*   var sections = [];//aqui solo seteo las diferentes partes de cada seccion, en un array de arrays
+       for (i = 0; i < temp_sections.length; i++) {
+           sections.push(temp_sections[i].split('---'));
+       }*/
 
+    for (i = 0; i < temp_sections.length; i++) {
+        var sectionOBJ = $("#section").clone(true);
+        setSection($(sectionOBJ), temp_sections[i].split('---'));
+        $(sectionOBJ).insertAfter($('#section:last')).show();
+    }
+
+
+    //setSection($(tempSectionOBJ), sections[0]);
 
 });
+
+function getSections(url) {
+    var tmp = null;
+    $.get({
+        'async': false,
+        'url': url,
+        'success': function(data) {
+            tmp = data.split('-NS-');
+        }
+    });
+    return tmp;
+};
+
+function setSection(object, data) {
+    $(object).find("#title").html(data[1]);
+    $(object).find("#description").html(data[2]);
+    $(object).find("#text").html(data[3]);
+    return false;
+};
