e0b27a43983c474a9d2313a51d11c12fe03fe5d1
Darian Lopez Utra <daroxs95@gmail.com>
2020-08-18 23:04:38 -0700

    READY auto lang selector

--- assets/lang_selector.js
+++ assets/lang_selector.js
@@ -1,14 +1,21 @@
 $(document).ready(function() {
+    let preferred_lang = navigator.languages[0].substring(0,2);
     let searchParams = new URLSearchParams(window.location.search)
-    var lang;
+    var lang_in_URLget;
     if (searchParams.has('lang')){
-
-        lang = searchParams.get('lang');
-
-        $("body").load('./'+lang+'.html #body',function(responseTxt, statusTxt, xhr){
+        lang_in_URLget = searchParams.get('lang');
+        $("body").load('./'+lang_in_URLget+'.html #body',function(responseTxt, statusTxt, xhr){
+            if(statusTxt == "error")
+                $("body").load('./' + preferred_lang + '.html #body',function(responseTxt, statusTxt, xhr){
+                    if(statusTxt == "error")
+                        $("body").load('./en.html #body');
+                });
+        });
+    }
+    else {
+        $("body").load('./' + preferred_lang + '.html #body',function(responseTxt, statusTxt, xhr){
             if(statusTxt == "error")
                 $("body").load('./en.html #body');
         });
     }
-    else $("body").load('./en.html #body');
 });
